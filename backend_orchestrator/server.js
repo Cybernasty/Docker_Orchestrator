22899767fde7061cab1fd5655b66c11cf5c5ff3c
Zitouni Montassar <zitounimontassar@gmail.com>
2025-03-06 12:41:14 +0100

    Initial commit: Orchestration project

---
+++ backend_orchestrator/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { connectDB } from "./config/db.js";
+
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("Server is ready")
+})
+
+console.log(process.env.MONGO_URI);
+
+app.listen(5000,() => {
+    connectDB();
+    console.log("Server started at http://localhost:5000 ");
+});
+
+
+
